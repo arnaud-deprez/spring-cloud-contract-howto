8489145f8cc05ac60fec039469ed018f6bc03248
Arnaud Deprez <arnaudeprez@gmail.com>
2018-06-29 17:25:21 +0200

    add howto with API compatibility check

---
+++ beer-api-consumer/src/main/java/com/powple/poc/BeerController.java
@@ -0,0 +1,97 @@
+package com.powple.poc;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+
+/**
+ * @author Marcin Grzejszczak
+ */
+@RestController
+class BeerController {
+
+	private final RestTemplate restTemplate;
+
+	int port = 8090;
+
+	BeerController(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	@RequestMapping(method = RequestMethod.POST,
+			value = "/beer",
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String gimmeABeer(@RequestBody Person person) throws MalformedURLException {
+		//remove::start[]
+		//tag::controller[]
+		ResponseEntity<Response> response = this.restTemplate.exchange(
+				RequestEntity
+						.post(URI.create("http://localhost:" + port + "/check"))
+						.contentType(MediaType.APPLICATION_JSON)
+						.body(person),
+				Response.class);
+		switch (response.getBody().status) {
+		case OK:
+			return "THERE YOU GO";
+		default:
+			return "GET LOST";
+		}
+		//end::controller[]
+		//remove::end[return]
+	}
+}
+
+class Person {
+	public String name;
+	public Integer age;
+
+	public Person(String name, Integer age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public Person() {
+	}
+}
+
+class Response {
+	public ResponseStatus status;
+}
+
+enum ResponseStatus {
+	OK, NOT_OK
+}
+
+
+//remove::start[]
+/*
+
+	@RequestMapping(method = RequestMethod.POST,
+			value = "/beer",
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String gimmeABeer(@RequestBody Person person) throws MalformedURLException {
+		ResponseEntity<Response> response = this.restTemplate.exchange(
+				RequestEntity
+						.post(URI.create("http://localhost:8090/check"))
+						.contentType(MediaType.APPLICATION_JSON)
+						.body(person),
+				Response.class);
+		switch (response.getBody().status) {
+		case OK:
+			return "THERE YOU GO";
+		default:
+			return "GET LOST";
+		}
+	}
+
+
+ */
+//remove::end[]
